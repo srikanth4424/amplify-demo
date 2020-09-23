64a20b1f907d2a6eb54162b10b888f9c76cdcb7a
VedaT706 <tveda24@gmail.com>
2020-09-23 15:40:07 +0530

    amp

---
+++ backend/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  application:
+  {
+    name: 'angular-starter',
+    angular: 'Angular 10.1.2',
+    bootstrap: 'Bootstrap 5.0.0',
+    fontawesome: 'Font Awesome 5.14.0',
+  },
+  url: 'http://localhost:5200',
+};
